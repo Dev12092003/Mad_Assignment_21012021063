d5c0b4254f37265aaf4bca9a0b3b29ee28349698
Dev <dev.12092003@gmail.com>
2023-10-25 23:54:35 +0530

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.mad_assignment"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mad_assignment"
